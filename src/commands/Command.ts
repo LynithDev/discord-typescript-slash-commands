59e6afe3b64df788cee543e4b0142da3d10bc315
Hot-Tutorials <Hot-Tutorials@users.noreply.github.com>
2021-08-17 22:48:05 +0200

    Added missing dependency and fixed ESLint erroring in files

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -1,13 +1,17 @@
-import { CommandContext } from "./CommandContext";
-import { SlashCommandBuilder } from '@discordjs/builders';
+/* eslint-disable no-useless-constructor */
+import { CommandContext } from './CommandContext';
 
 export default abstract class Command {
+    // eslint-disable-next-line no-empty-function
     constructor(public name: string, public description: string) { }
 
     abstract execute(ctx: CommandContext)
 
-    addOptions() {
+    static addOptions() {
         return [];
     }
 
+    static addSubcommands() {
+        return [];
+    }
 }
--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,3 +1,1 @@
-export const capitalizeString = (string: string) => {
-    return string.split(" ").map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(" ");
-}
+export const capitalizeString = (string: string) => string.split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
